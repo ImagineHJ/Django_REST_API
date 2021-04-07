ee11a2b00099f5005f8422f5017258d2f5162e7d
Haejin <hj0816hj@gmail.com>
2021-04-07 17:44:48 +0900

    modify view to CBV

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('posts/', views.post_list)
+    path('posts/', views.PostList.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,16 @@
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+# from django.http import JsonResponse
+# from django.views.decorators.csrf import csrf_exempt
+# from rest_framework.parsers import JSONParser
 from .models import Post
 from .serializers import PostSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+'''
+# Use CVB instead of FCV
 
 @csrf_exempt
 def post_list(request):
@@ -25,3 +31,19 @@ def post_list(request):
             serializer.save()  # save to DB
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+'''
+
+
+class PostList(APIView):
+
+    def get(self, request, format=None):
+        post = Post.objects.all()  # get queryset of the Post
+        serializer = PostSerializer(post, many=True)  # Serialize it to python native data type
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
