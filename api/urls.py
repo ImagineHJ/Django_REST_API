e394ae6761b867d50516a31793ce6fd318e25339
Haejin <hj0816hj@gmail.com>
2021-04-07 18:17:34 +0900

    add postdetail view

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
 urlpatterns = [
-    path('posts/', views.PostList.as_view())
+    path('posts/', views.PostList.as_view()),
+    path('posts/<int:pk>', views.PostDetail.as_view())
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -33,17 +33,55 @@ def post_list(request):
         return JsonResponse(serializer.errors, status=400)
 '''
 
-
+# def post_list(request):
 class PostList(APIView):
 
+    # if request.method == 'GET':
     def get(self, request, format=None):
+
         post = Post.objects.all()  # get queryset of the Post
         serializer = PostSerializer(post, many=True)  # Serialize it to python native data type
+
+        # return JsonResponse(serializer.data, safe=False)  # response with JSON
         return Response(serializer.data)
 
+    # elif request.method == 'POST':
     def post(self, request, format=None):
-        serializer = PostSerializer(data=request.data)
+        # data = JSONParser().parse(request)  # parse the JSON data
+        # serializer = PostSerializer(data=data)
+
+        serializer = PostSerializer(data=request.data)  # Handles arbitrary data.  Works for 'POST', 'PUT' and 'PATCH' methods
+        if serializer.is_valid():
+            serializer.save()  # save to DB
+
+            # return JsonResponse(serializer.data, status=201)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)  # Renders to content type as requested by the client
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
